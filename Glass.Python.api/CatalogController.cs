55fdb0d8b17ce6f89df9025d75e488d52633fa8e
Anthony Formichelli <formichelli.7@osu.edu>
2024-03-22 18:34:11 -0400

    basic REST calls

--- Glass.Python.api/CatalogController.cs
+++ Glass.Python.api/CatalogController.cs
@@ -9,7 +9,6 @@ namespace Glass.Python.Api.Controllers
     public class CatalogController : ControllerBase
     {
         [HttpGet]
-        [Route("/catalog")]
         public IActionResult GetItems()
         {
             var items = new List<Item>()
@@ -19,6 +18,41 @@ namespace Glass.Python.Api.Controllers
             };
 
             return Ok(items);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetItem(int id)
+        {
+            Item item = new Item("Shirt", "Ohio State shirt.", "nike", 29.99m);
+            item.Id = id;
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Item item)
+        {
+            return Created("/catalog/42", item);
+        }
+
+        [HttpPost("{id:int}/ratings")]
+        public IActionResult PostRating(int id, [FromBody] Rating rating)
+        {
+            Item item = new Item("Shirt", "Ohio State shirt.", "nike", 29.99m);
+            item.Id = id;
+            item.AddRating(rating);
+            return Ok(item);
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult Put(int id, Item item)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id, Item item)
+        {
+            return NoContent();
         }
     }
 }
