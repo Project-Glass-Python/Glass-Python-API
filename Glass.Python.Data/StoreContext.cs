f427b7c7f7d6afe603e2fb56459c536101cfaf61
Anthony Formichelli <formichelli.7@osu.edu>
2024-03-24 17:48:30 -0400

    pulled

---
+++ Glass.Python.Data/StoreContext.cs
@@ -0,0 +1,20 @@
+﻿using Glass.Python.Domain.Catalog;
+using Microsoft.EntityFrameworkCore;
+
+namespace Glass.Python.Data;
+
+public class StoreContext : DbContext
+{
+    public StoreContext(DbContextOptions<StoreContext> options)
+        : base(options)
+    { }
+
+    public DbSet<Item> Items { get; set; }
+    public DbSet<Rating> Ratings { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        DbInitializer.Initialize(builder);
+    }
+}
