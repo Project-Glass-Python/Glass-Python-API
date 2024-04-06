8f9d6dfdcab83785e9501f2c6e64a1fc6fd41e37
Anthony Formichelli <formichelli.7@osu.edu>
2024-04-06 11:20:00 -0400

    Backend tests created

--- tests/Glass.Python.Domain.Tests/RatingTests.cs
+++ tests/Glass.Python.Domain.Tests/RatingTests.cs
@@ -18,4 +18,40 @@ public class RatingTests
         Assert.AreEqual("Mike", rating.UserName);
         Assert.AreEqual("Great fit!", rating.Review);
     }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void Cannot_Create_Rating_With_Invalid_Starts()
+    {
+        var rating = new Rating(0, "Mike", "Great fit!");
+    }
+
+    [TestMethod]
+    public void Can_Create_New_Item()
+    {
+        //Arrange
+        var item = new Item("Name", "Description", "Brand", 10.00m);
+
+        //Act (empty)
+
+        //Assert
+        Assert.AreEqual("Name", item.Name);
+        Assert.AreEqual("Description", item.Description);
+        Assert.AreEqual("Brand", item.Brand);
+        Assert.AreEqual(10.00m, item.Price);
+    }
+
+    [TestMethod]
+    public void Can_Create_Add_Rating()
+    {
+        //Arrange
+        var item = new Item("Name", "Description", "Brand", 10.00m);
+        var rating = new Rating(5, "Name", "Review");
+
+        //Act (empty)
+        item.AddRating(rating);
+
+        //Assert
+        Assert.AreEqual(rating, item.Ratings[0]);
+    }
 }
